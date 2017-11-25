734ac0729e29fc97e5120c883dc348cdec7d69b9
arthur32810 <arthur32810@hotmail.fr>
2017-11-25 14:56:12 +0100

    Ajout du titre

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -2,7 +2,11 @@
 
 <?php ob_start(); ?>
 
+	<h1> <?= htmlspecialchars($post['title']) ?> </h1>
+
+	<a href="index.php?action=listPosts"> Revenir à l'index </a>
 	
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('view/template.php');?>
