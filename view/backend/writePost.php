8695040637b60534090ec388171a757a56791d4e
arthur32810 <arthur32810@hotmail.fr>
2017-11-25 16:14:45 +0100

    Ajout des principales fonctionnalité de la page de création de chapitre

--- view/backend/writePost.php
+++ view/backend/writePost.php
@@ -1,6 +1,24 @@
-<?php $title ='Création d\'un chapitre' ?>
+<?php $title ='Création d\'un chapitre' ;
+	$script = ' <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+  		<script>tinymce.init({ selector:\'textarea\' });</script>'?>
 
 <?php ob_start(); ?>
+	
+	<h1> Ecriture d'un chapitre </h1>
+
+	<form action="index.php?action=create_post" method="post">
+		<div>
+			<label for="author">Titre</label><br />
+			<input type="text" id="author" name="author" />
+		</div> <br/>
+		<div>
+			<label for="comment">Texte</label><br />
+			<textarea id="comment" name="comment"></textarea>
+		</div> <br/>
+		<div>
+			<input type="submit" />
+		</div>
+	</form>
 
 <?php $content = ob_get_clean(); ?>
 
