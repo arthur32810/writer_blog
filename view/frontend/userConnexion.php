683fbbe5ca60651905cf9c6b9e70315e1baf1e65
arthur32810 <arthur32810@hotmail.fr>
2017-11-25 17:58:08 +0100

    Ajout du formulaire sur la page de connexion

--- view/frontend/userConnexion.php
+++ view/frontend/userConnexion.php
@@ -2,6 +2,27 @@
 
 <?php ob_start(); ?>
 
+	<form action="index.php?action=connexion" method="post">
+		<div>
+			<label for="pseudo">Pseudo</label>
+			<input type="text" id="pseudo" name="pseudo" />
+		</div>
+
+		<div>
+			<label for="pass">Mot de passe :</label>
+			<input type="text" id="pass" name="pass" />
+		</div>
+
+		<div>
+			<label for="cookie">Maintenir la connexion</label>
+			<input type="checkbox" id="cookie" name="cookie" />
+		</div>
+
+		<div>
+			<input type="submit" />
+		</div>
+	</form>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('view/template.php');?>
