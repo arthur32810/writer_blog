63b2a551fb98e05d7ead69cb641dec13cbfd002b
arthur32810 <arthur32810@hotmail.fr>
2017-11-25 14:52:09 +0100

    ajout du lien 'lire le chapitre'

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -18,7 +18,7 @@
                 <p>
                     <?= nl2br(htmlspecialchars($data['content'])) ?>
                     <br />
-                    <em><a href="index.php?action=post&&id=<?= $data['id']?>">Commentaires</a></em>
+                    <em><a href="index.php?action=post&&id=<?= $data['id']?>"> Lire le chapitre</a></em>
                 </p>
             </div>
 			
