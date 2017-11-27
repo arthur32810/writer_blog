af0f3d6701a01d36eddb3745eac4e5e4fd82042c
arthur32810 <arthur32810@hotmail.fr>
2017-11-27 11:19:00 +0100

    Rajout de bootstrap dans le template

--- view/template.php
+++ view/template.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8" />
         <title><?= $title ?></title>
         <link href="public/css/style.css" rel="stylesheet" /> 
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
         <?php if(!empty($script)){ echo $script;} ?>
     </head>
         
